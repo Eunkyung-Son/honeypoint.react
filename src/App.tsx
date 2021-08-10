db921281a1bcbeb0d2f0e8840b43a0f3f0936c06
ek.son <ek.son@doctorkeeper.com>
2021-08-10 17:57:35 +0900

    클린 코드

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import './pages/MainPage'
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/login/LoginPage'
 import SignupPage from './pages/signup/SignupPage';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path='/' exact component={MainPage} />
-        <Route path='/login' component={LoginPage} />
-        <Route path='/signup' component={SignupPage} />
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route path='/' exact component={MainPage} />
+      <Route path='/login' component={LoginPage} />
+      <Route path='/signup' component={SignupPage} />
+    </Switch>
   );
 }
 
