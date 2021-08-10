40c8a838590cff21e84f238f64ba605a410c1900
ek.son <ek.son@doctorkeeper.com>
2021-08-10 11:00:20 +0900

    Member 데이터 모델 추가

---
+++ src/models/Member.ts
@@ -0,0 +1,62 @@
+type Member = {
+    bCategory: number;
+    bEnrollDate: Date;
+    bNo: number;
+    bTitle: string;
+    bType: string;
+    mAddress: string;
+    bBirthday: Date;
+    mEmail: string;
+    mEnrollDate: Date;
+    mGrad: string;
+    mId: string;
+    mName: string;
+    mNickname: string;
+    mNo: number;
+    mOriginPf: string;
+    mPhone: string;
+    mPoint: number;
+    mPwd: string;
+    mSortNo: number;
+    mStatus: string;
+    mStrePf: string;
+    pointDate: Date;
+    pointHistory: number;
+    rConf: number;
+    rName: string;
+    rNo: number;
+    resveAmount: number;
+    rsvDe: Date;
+    setleDe: Date;
+    stPrc: number;
+    visitrCo: number;
+}
+
+export default Member;
+
+
+
+
+
+/**
+ * 
+ * 
+ * 	private int mNo;
+	private String mId;
+	private String mPwd;
+	private String mName;
+	private String mNickname;
+	private String mBirthday;
+	private String mEmail;
+	private String mPhone;
+	private String mAddress;
+	private Date mEnrollDate;
+	private Date mModifyDate;
+	private String mGrad;
+	private int mPoint;
+	private char mStatus;
+	private int rConf;
+	private String mOriginPf;
+	private String mStrePf;	
+	private int mSortNo;
+ */
