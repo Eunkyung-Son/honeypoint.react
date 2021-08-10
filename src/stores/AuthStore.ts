943d02977344644a117b77c682db4f2814b46171
ek.son <ek.son@doctorkeeper.com>
2021-08-10 10:50:39 +0900

    로그인 상태 체크를 위한 AuthStore 추가

---
+++ src/stores/AuthStore.ts
@@ -0,0 +1,25 @@
+import { action, observable } from "mobx";
+import RootStore from "./RootStore";
+
+export default class AuthStore {
+  root: RootStore;
+  @observable isLoggedIn = false;
+  
+  constructor(root: RootStore) {
+    this.root = root;
+    this.init();
+  }
+
+  @action.bound
+  init = () => {
+    if (localStorage.getItem('memberId')) {
+      this.isLoggedIn = true;
+    }
+  }
+
+  @action.bound
+  setIsLoggedIn(isLoggedIn: boolean) {
+    this.isLoggedIn = isLoggedIn;
+  }
+
+}
--- src/stores/RootStore.ts
+++ src/stores/RootStore.ts
@@ -1,10 +1,12 @@
 import { RouterStore } from "mobx-react-router";
-
+import AuthStore from "./AuthStore";
 class RootStore {
 	routing: RouterStore;
+	authStore: AuthStore;
 
 	constructor() {
 		this.routing = new RouterStore();
+		this.authStore = new AuthStore(this);
 	}
 }
 
