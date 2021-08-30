cc1161e81b04e45b0c92efd85d9a4506b61ad9c1
Eunkyung-Son <sek9510@naver.com>
2021-08-30 16:06:49 +0900

    mypage ui 반응형으로 수정

--- src/pages/mypage/MyInfoPage.tsx
+++ src/pages/mypage/MyInfoPage.tsx
@@ -1,5 +1,6 @@
-import { Card, Space } from "antd";
 import React from "react";
+import { Card, Col, Row } from "antd";
+import './MyInfoPage.scss';
 
 type Props = {
 
@@ -9,39 +10,41 @@ export default class MyInfoPage extends React.Component<Props> {
   render() {
     return (
       <div className="content-area">
-        <Space>
-          <div className="site-card-border-less-wrapper">
-            <Card title="기본 정보 변경" bordered={false} style={{ width: 500 }}>
+        <Row gutter={[24, 10]}>
+          <Col span={3}></Col>
+          <Col span={9}>
+            <Card title="기본 정보 변경" bordered={false}>
               <p>Card content</p>
               <p>Card content</p>
               <p>Card content</p>
             </Card>
-          </div>
-          <div className="site-card-border-less-wrapper">
-            <Card title="비밀번호 변경" bordered={false} style={{ width: 500 }}>
-              <p>Card content</p>
-              <p>Card content</p>
-              <p>Card content</p>
-            </Card>
-          </div>
-        </Space>
-        <div></div>
-        <Space>
-          <div className="site-card-border-less-wrapper">
-            <Card title="내 활동 내역" bordered={false} style={{ width: 500 }}>
-              <p>Card content</p>
-              <p>Card content</p>
-              <p>Card content</p>
-            </Card>
-          </div>
-          <div className="site-card-border-less-wrapper">
-            <Card title="회원 탈퇴" bordered={false} style={{ width: 500 }}>
-              <p>Card content</p>
-              <p>Card content</p>
-              <p>Card content</p>
-            </Card>
-          </div>
-        </Space>
+          </Col>
+          <Col span={9}>
+          <Card title="비밀번호 변경" bordered={false}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+          </Col>
+          <Col span={3}></Col>
+          <Col span={3}></Col>
+          <Col span={9}>
+          <Card title="내 활동 내역" bordered={false}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+          </Col>
+
+          <Col span={9}>
+          <Card title="회원 탈퇴" bordered={false}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+          </Col>
+          <Col span={3}></Col>
+        </Row>
       </div>
     )
   }
