879ba106beee5c9ce9b97f8335c77e8d5d88ef4e
Eunkyung-Son <sek9510@naver.com>
2021-10-05 22:37:55 +0900

    게시판 검색 기능 수정중

--- src/pages/board/BoardPage.tsx
+++ src/pages/board/BoardPage.tsx
@@ -1,5 +1,5 @@
 import { observer } from "mobx-react";
-import { Table, Tabs } from 'antd';
+import { Col, Row, Select, Space, Table, Tabs } from 'antd';
 import Search from "antd/lib/input/Search";
 import { useEffect, useState } from "react";
 import { SERVER_URL } from "../../config/config";
@@ -11,16 +11,37 @@ type Props = {
 }
 
 const BoardPage:React.FC<Props> = (props: Props) => {
-
   const [boardPageStore] = useState(() => new BoardPageStore());
+  const [searchCondition, setSearchCondition] = useState('');
   const { TabPane } = Tabs;
 
-  const callback = (key: any) => {
-    console.log(key);
+  const handleKeyChange = (key: string) => {
+    fetchBoards(Number(key));
   }
 
-  const onSearch = () => {
+  const onSearch = async (values: any) => {
+    console.log(values);
+    console.log(searchCondition);
+    const URL = `${SERVER_URL}/api/searchBoards/1`;
+    const params = {
+      searchOption: {
+        condition: searchCondition,
+        value: values
+      }
+    };
 
+    console.log(params);
+    await axios
+      .get(URL, {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        params: {
+          ...params
+        }
+      }).then((response: AxiosResponse) => {
+        console.log(response);
+      })
   }
 
   const fetchBoards = async (boardType?: number) => {
@@ -70,27 +91,60 @@ const BoardPage:React.FC<Props> = (props: Props) => {
     }
   ];
 
-  return (
-    <>
-      게시판 페이지 입니다.
+  const handleSearchConditionChange = (value: string) => {
+    console.log(`selected ${value}`);
+    setSearchCondition(value);
+  }
 
-      <Tabs defaultActiveKey="1" onChange={callback}>
+  const { Option } = Select;
+
+  const operations = (          
+    <Row>
+      <Col span={24}>
+        <Space>
+          <Select defaultValue="all" style={{ width: 120 }} onChange={handleSearchConditionChange}>
+            <Option value="all">전체</Option>
+            <Option value="writer">작성자</Option>
+            <Option value="title">제목</Option>
+            <Option value="content">내용</Option>
+          </Select>
+          <Search 
+            placeholder="검색할 내용을 입력하세요" 
+            onSearch={onSearch} 
+            enterButton
+            style={{width: "50px;"}}
+          />
+        </Space>
+      </Col>
+    </Row>
+  );
+
+
+  return (
+    <div style={{margin: "2%"}}>
+      <h1>커뮤니티 게시판</h1>
+
+      <Tabs defaultActiveKey="1" onChange={handleKeyChange} tabBarExtraContent={operations}>
         <TabPane tab="지역별" key="1">
-          <Search placeholder="input search text" onSearch={onSearch} enterButton />
           <Table 
             columns={columns} 
             dataSource={boardPageStore.boardList} 
           />
-    
         </TabPane>
         <TabPane tab="주제별" key="2">
-          Content of Tab Pane 2
+          <Table 
+            columns={columns} 
+            dataSource={boardPageStore.boardList} 
+          />
         </TabPane>
         <TabPane tab="자유게시판" key="3">
-          Content of Tab Pane 3
+          <Table 
+            columns={columns} 
+            dataSource={boardPageStore.boardList} 
+          />
         </TabPane>
       </Tabs>
-    </>
+    </div>
   )
 }
 
