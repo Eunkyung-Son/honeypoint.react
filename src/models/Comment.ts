ae3704b926b8d60417ef454f7e6685dece93b13a
Eunkyung-Son <sek9510@naver.com>
2021-10-15 18:45:18 +0900

    데이터 모델 추가

---
+++ src/models/Comment.ts
@@ -0,0 +1,13 @@
+type Comment = {
+  bNo: number,
+  cmtContent: string,
+  cmtEnrollDate: string,
+  cmtModifyDate: string,
+  cmtNo: number,
+  cmtStatus: string,
+  mId: string,
+  mNickname: string,
+  mNo: number
+}
+
+export default Comment;
