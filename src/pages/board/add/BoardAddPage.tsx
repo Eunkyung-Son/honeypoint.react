2f3a4bca9be430efd5dbf0726c03464062a37014
Eunkyung-Son <sek9510@naver.com>
2021-10-26 23:37:48 +0900

    게시글 작성 기능 추가

---
+++ src/pages/board/add/BoardAddPage.tsx
@@ -0,0 +1,102 @@
+import { Button, Col, Form, Input, Row, Radio, Select, RadioChangeEvent } from "antd";
+import axios, { AxiosResponse } from "axios";
+import { observer } from "mobx-react";
+import React, { useState } from "react";
+import { SERVER_URL } from "../../../config/config";
+import { useRootStore } from "../../../hooks/StoreContextProvider";
+
+const layout = {
+  wrapperCol: { span: 18 },
+};
+
+const { Option } = Select;
+
+const selectOptions = [
+  ['서울', '경기', '인천', '강원', '경상', '전라', '충청', '제주', '해외'],
+  ['데이트', '먹방', '골목식당'],
+  ['잡담', '맛집공유', '일상', '만남']
+];
+
+const BoardAddPage: React.FC = () => {
+  const [form] = Form.useForm();
+  const { authStore, routing } = useRootStore();
+  const [radioValue, setRadioValue] = useState<string>('1');
+  const onBoardWrite = async (values: any) => {
+    console.log(values);
+    const URL = `${SERVER_URL}/api/board/insert`;
+    await axios
+      .post(URL,
+        {
+          mNo: authStore.member?.mNo,
+          bType: values.bType,
+          bCategory: values.bCategory || '',
+          bTitle: values.bTitle,
+          bContent: values.bContent
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+        })
+      .then((response: AxiosResponse) => {
+        routing.push("/board");
+      })
+  }
+
+  const onRadioChange = (e: RadioChangeEvent) => {
+    setRadioValue(e.target.value);
+    console.log(e.target.value);
+    console.log(radioValue)
+  }
+
+  return (
+    <div style={{ margin: "2%" }}>
+      <Row>
+        <Col span={24}>
+          <h1>커뮤니티 게시판</h1>
+        </Col>
+      </Row>
+      <Form
+        {...layout}
+        form={form}
+        name="nest-messages"
+        onFinish={onBoardWrite}
+      >
+        <Form.Item
+          initialValue="1"
+          name={["bType"]}
+        >
+          <Radio.Group onChange={onRadioChange}>
+            <Radio.Button value="1">지역별</Radio.Button>
+            <Radio.Button value="2">주제별</Radio.Button>
+            <Radio.Button value="3">자유게시판</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+        {['1', '2'].includes(radioValue) && (
+          <Form.Item
+            name={["bCategory"]}
+          >
+            <Select placeholder="카테고리">
+              {selectOptions[Number(radioValue) - 1] && selectOptions[Number(radioValue) - 1].map(option => (
+                <Option value={option}>{option}</Option>
+              ))}
+            </Select>
+          </Form.Item>
+        )}
+        <Form.Item name="bTitle">
+          <Input placeholder={"제목을 입력해주세요."} />
+        </Form.Item>
+        <Form.Item name="bContent">
+          <Input.TextArea />
+        </Form.Item>
+        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+          <Button type="primary" htmlType="submit">
+            글쓰기
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  )
+}
+
+export default observer(BoardAddPage);
