8adce36f3a5b87d2eb6e305fa35758f0b1001ea6
Eunkyung-Son <sek9510@naver.com>
2021-11-01 21:43:56 +0900

    레스토랑 회원 마이페이지 ui 추가

--- src/pages/mypage/restaurant/RestaurantMyInfoPage.tsx
+++ src/pages/mypage/restaurant/RestaurantMyInfoPage.tsx
@@ -1,117 +1,51 @@
-import { useEffect, useState } from "react";
 import { observer } from "mobx-react";
-import axios, { AxiosResponse } from "axios";
-import { Button, message, Upload } from "antd";
-import { UploadOutlined } from '@ant-design/icons';
-import { SERVER_URL } from "../../../config/config";
-import { useRootStore } from "../../../hooks/StoreContextProvider";
-import RestaurantMyInfoStore from "./RestaurantMyInfoStore";
-import { RcFile } from "antd/lib/upload";
+import { Card, Col, Row } from "antd";
+import { Link } from "react-router-dom";
 
-type Props = {
-
-}
-
-const RestaurantMyInfoPage: React.FC<Props> = observer((props: Props) => {
-  const { authStore } = useRootStore();
-  const [restaurantMyInfoStore] = useState(() => new RestaurantMyInfoStore());
-  const [fileList, setFileList] = useState<RcFile[]>([]);
-  const [uploading, setUploading] = useState(false);
-  console.log(authStore.member)
-
-  useEffect(() => {
-    restaurantInfo();
-    console.log(authStore);
-  }, [])
-
-  const restaurantInfo = async () => {
-    if (!authStore.member) return;
-    console.log(authStore, "dsffsdsdf");
-    console.log(authStore.member);
-    const URL = `${SERVER_URL}/api/restaurantByMember/${authStore.member?.mNo}`;
-    await axios
-      .get(URL,
-        {
-          headers: {
-            'Content-Type': 'application/json'
-          },
-        }
-      )
-      .then((response: AxiosResponse) => {
-        console.log(response);
-        restaurantMyInfoStore.setRestaurantData(response.data.restaurant);
-        console.log(restaurantMyInfoStore.restaurantData);
-      })
-  }
-
-  const handleUpload = async () => {
-    const formData = new FormData();
-    fileList.forEach(file => {
-      formData.append('files', file);
-    });
-
-    console.log(formData);
-
-    setUploading(true);
-
-    const URL = `${SERVER_URL}/api/file/restaurant/${restaurantMyInfoStore.restaurantData?.rNo}`;
-    console.log(formData);
-      await axios
-        .post(URL,
-          
-          formData
-          ,{
-            headers: {
-              'content-type': 'multipart/form-data'
-            },
-          }
-        )
-        .then((response: AxiosResponse) => {
-          console.log(response);
-          setFileList([]);
-          setUploading(false);
-          message.success('upload successfully.');
-        }).catch((error) => {
-          message.error('upload failed.');
-        })
-
-  }
-  
-  const uploadProps = {
-    onRemove: (file: any) => {
-      const index = fileList.indexOf(file);
-      const newFileList = fileList.slice();
-      newFileList.splice(index, 1);
-      return setFileList(newFileList);
-    },
-    beforeUpload: (file: RcFile, inputFileList: RcFile[]) => {
-      if (inputFileList.length) {
-        setFileList([...fileList, ...inputFileList]);
-      } else {
-        setFileList([...fileList, file]);
-      }
-      return false;
-    },
-    fileList,
-    multiple: true,
-  };
+const RestaurantMyInfoPage: React.FC = () => {
 
   return (
-    <>
-      <Upload {...uploadProps}>
-        <Button icon={<UploadOutlined />}>Select File</Button>
-      </Upload>
-      <Button
-        type="primary"
-        onClick={handleUpload}
-        disabled={fileList.length === 0}
-        loading={uploading}
-        style={{ marginTop: 16 }}
-      >
-        {uploading ? 'Uploading' : 'Start Upload'}
-      </Button>
-    </>
+    <div className="content-area">
+      <Row gutter={[24, 10]}>
+        <Col span={3}></Col>
+        <Col span={9}>
+          <Card title="레스토랑 사진 등록" bordered={false}>
+            <div>
+              서비스 사용자에게 보여줄 레스토랑 사진을 등록하거나 수정할 수 있습니다.
+            </div>
+            <Link to="/mypage/restaurant/file">
+              <p>등록하기</p>
+            </Link>
+          </Card>
+        </Col>
+        <Col span={9}>
+        <Card title="비밀번호 변경" bordered={false}>
+          <div>
+            HONEYPOINT 로그인 시 사용하는 비밀번호를 변경할 수 있습니다.<br />
+            주기적인 비밀번호 변경을 통해 개인정보를 안전하게 보호하세요.
+          </div>
+          <br />
+          <Link to="/mypage/password"><p>비밀번호 변경</p></Link>
+        </Card>
+        </Col>
+        <Col span={3}></Col>
+        <Col span={3}></Col>
+        <Col span={9}>
+        <Card title="레스토랑 정보 수정" bordered={false}>
+          <p>나의 레스토랑 정보를 수정할 수 있습니다.</p>
+          <Link to="/mypage/restaurant/edit"><p>수정하기</p></Link>
+        </Card>
+        </Col>
+        <Col span={9}>
+        <Card title="회원 탈퇴" bordered={false}>
+          <p>HONEYPOINT 회원을 탈퇴합니다.</p>
+          <Link to="/mypage/withdrawal"><p>탈퇴하기</p></Link>
+        </Card>
+        </Col>
+        <Col span={3}></Col>
+      </Row>
+    </div>
   )
-})
+}
 
-export default RestaurantMyInfoPage
+export default observer(RestaurantMyInfoPage)
